8bee4fe6fd11aeca993861ddd619587ddda1fc32
Emerson Valentim <69153761+Emerson-Valentim@users.noreply.github.com>
2021-09-11 16:22:33 -0300

    Config/initial (#1)
    * feat: add router config
    
    * feat: setup prettier and eslint

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store } from './hooks/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/routes/Router.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Login } from '../app/Login/Login';
+
+import { Counter } from '../features/counter/Counter';
+
+export function MainRouter(): JSX.Element {
+	return (
+		<div>
+			<Router>
+				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/">
+						<Counter />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
+}
