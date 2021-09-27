5cd731ccd9c28a978eb26e1652221e22048dafd0
Emerson Valentim <emevalentim0@gmail.com>
2021-09-27 15:19:14 -0300

    feat: add custom carousel butons

--- src/app/Home/components/Introduction/styles.ts
+++ src/app/Home/components/Introduction/styles.ts
@@ -1,27 +1,36 @@
-import { Carousel } from "antd"
 import styled from "styled-components"
 
 export const MainWrapper = styled.div`
   display: flex;
+
   justify-content: center;
   align-items: center;
-  min-height: 80%;
 
-  padding: 1em;
+  background-color: green;
+
+  height: 100vh;
 `
 
-export const WelcomeCarousel = styled.div`
-  max-width: 100%;
-  min-height: 100%;
+const commonSize = {
+  height: '1vh',
+  width: '4vh',
+  'border-radius': '10px'
+}
 
+export const WelcomeCarousel = styled.div`
   .ant-carousel {
     .slick-slider {
       ul {
-        li {
-          background: green;
-        }
+        li {            
+          background: white;
 
-        .slick-active {
+          ${commonSize}
+
+          button {
+            background: gray;
+
+            ${commonSize}
+          }
         }
       }
     }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -1,7 +1,5 @@
 import styled from "styled-components"
 
 export const MainWrapper = styled.div`
-  padding: 1em;
-
   min-width: 100%;
 `
