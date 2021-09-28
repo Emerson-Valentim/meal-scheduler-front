963b68b541f386380ea9b3d884d1a11ab9aec852
Emerson Valentim <emevalentim0@gmail.com>
2021-09-27 21:40:11 -0300

    refactor: change carousel to external component

---
+++ src/components/CustomCarousel/style.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export interface CarouselStyle {
+  height: string
+  width: string
+  'border-radius'?: string
+}
+
+export const StyledCarousel = styled.div`
+
+  width: 100% !important;
+
+  .ant-carousel {
+    .slick-slider {
+      ul {
+        li {            
+          background: white;
+
+          ${props => props.style}
+
+          button {
+            background: gray;
+
+            ${props => props.style}
+          }
+        }
+      }
+    }
+    .slick-list {
+      .slick-slide {
+        div {
+          div {
+            display: flex !important;
+          }
+        }
+      }
+    }
+  }
+`
