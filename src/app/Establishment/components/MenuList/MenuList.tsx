4e4224530c1e8198f685c408a3395555fb4269d5
Emerson Valentim <emevalentim0@gmail.com>
2021-09-28 10:42:22 -0300

    feat: add table list and menu list components

---
+++ src/app/Establishment/components/MenuList/MenuList.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function MenuList() {
+  return (<div>Lista de Menu</div>)
+}
---
+++ src/app/Establishment/components/TableList/TableList.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function TableList() {
+  return (<div>Lista de Mesas</div>)
+}
