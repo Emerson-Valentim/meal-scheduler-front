75a8f9852e6ebb0fe0eee28d6f7c91b7451ec575
Emerson Valentim <emevalentim0@gmail.com>
2021-09-29 18:45:22 -0300

    feat: add schedule styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 import { MainRouter } from './routes/Router';
 import { AlertBox } from './components/AlertBox/AlertBox';
