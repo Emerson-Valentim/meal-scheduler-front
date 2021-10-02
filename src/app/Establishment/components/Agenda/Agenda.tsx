9e518aec39fa3b9123b4b1d17a17fce5e5c6ccb4
Emerson Valentim <emevalentim0@gmail.com>
2021-10-02 20:42:10 -0300

    feat: change component and add calendar description

--- src/app/Establishment/components/Agenda/Agenda.tsx
+++ src/app/Establishment/components/Agenda/Agenda.tsx
@@ -1,23 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useState } from 'react'
 
-import { CustomScheduleComponent, MainWrapper } from './styles'
+import { AgendaDescription, CreateEventModal, CustomAgenda, MainWrapper, WorkingDayDescription } from './styles'
 import { useAppDispatch, useAppSelector } from '../../../../hooks/hooks'
 import { loadSchedule } from '../../../../hooks/Schedule';
-
-import {
-  WorkWeek,
-  Inject,
-  ViewsDirective,
-  ViewDirective,
-  ScheduleComponent
-} from '@syncfusion/ej2-react-schedule';
-
-import { extend } from '@syncfusion/ej2-base';
 
 import { DateTime } from 'luxon';
 import { loadReservations, setReservationInterval } from '../../../../hooks/Reservation';
 import { updateLoading } from '../../../../hooks/Common';
+
+require('moment/locale/pt.js');
 
 export type ReservationDefinition = {
   establishment_id?: number
@@ -30,6 +22,10 @@ const formatDate = (date: Date): DateTime => DateTime.fromJSDate(new Date(date))
 
 export function Agenda({ schedule: scheduleId }: ReservationDefinition) {
 
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   const dispatch = useAppDispatch();
 
   const schedule = useAppSelector(state => state.schedule.load.filtered);
@@ -37,64 +33,47 @@ export function Agenda({ schedule: scheduleId }: ReservationDefinition) {
 
   const { establishment, table } = useAppSelector(state => state.reservation.create.params)
 
-  const workingDays = useCallback((): number[] => {
-    dispatch(updateLoading(true))
-    if (schedule?.state === 'ok' && schedule?.data.definition) {
-      dispatch(updateLoading(false))
-      return Object.keys(schedule?.data.definition).map(day => +day)
-    }
-    return [1]
-  }, [schedule]);
+  const [visible, setVisible] = useState(false)
 
-  const getCurrentReservations = (reservations) => {
-    return reservations.data.map(({ id, interval: { end, start } }) => ({
-      Id: id,
-      Subject: `Reserva ${id}`,
-      StartTime: DateTime.fromISO(start).toJSDate(),
-      EndTime: DateTime.fromISO(end).toJSDate()
-    }))
+  const colors = {
+    'currentUser': "rgba(102, 195, 131, 1)",
+    "otherUser": "rgba(242, 177, 52, 1)",
   }
 
-  const getEventSettings = useCallback((reservationList = []): any => {
-    if (reservations?.state === 'ok') {
-      reservationList = getCurrentReservations(reservations)
+  const workingDays = useCallback((): JSX.Element[] => {
+    dispatch(updateLoading(true))
+    if (schedule?.state === 'ok' && schedule?.data?.definition) {
+      dispatch(updateLoading(false))
+      return Object.entries(schedule?.data.definition).map(([day, interval]) => (
+        <WorkingDayDescription key={`schedule-${day}`}>
+          <p>{capitalize(DateTime.local().set({ weekday: +day }).weekdayLong)}: {interval.start} - {interval.end}</p>
+        </WorkingDayDescription>
+      ))
     }
-    return extend([], reservationList, undefined, true);;
+    return []
+  }, [schedule]);
+
+  const getCurrentReservations = useCallback(() => {
+    return reservations.data.map(({ id, interval: { end, start } }) => ({
+      _id: `reservation-${id}`,
+      name: `Reserva ${id}`,
+      startDateTime: DateTime.fromISO(start).toJSDate(),
+      endDateTime: DateTime.fromISO(end).toJSDate(),
+      classes: 'currentUser'
+    }))
   }, [reservations])
 
-  const handleEvent = useCallback((event) => {
-    switch (event.type) {
-      case 'QuickInfo':
-        const { StartTime, EndTime } = event.data;
+  const createReservation = useCallback((event) => {
+    setVisible(true)
+    // const { StartTime, EndTime } = event.data;
 
-        const interval = {
-          start: formatDate(StartTime).plus({ milliseconds: 1 }).toString(),
-          end: formatDate(EndTime).minus({ milliseconds: 1 }).toString()
-        }
+    // const interval = {
+    //   start: formatDate(StartTime).plus({ milliseconds: 1 }).toString(),
+    //   end: formatDate(EndTime).minus({ milliseconds: 1 }).toString()
+    // }
 
-        dispatch(setReservationInterval(interval))
-
-        break
-      case 'ViewEventInfo':
-        event.cancel = true
-        break
-    }
+    // dispatch(setReservationInterval(interval))
   }, [reservations])
-
-  const handleConfig = useCallback((scheduleConfig: ScheduleComponent) => {
-    if (scheduleConfig) {
-      scheduleConfig.eventSettings.allowDeleting = false
-      scheduleConfig.eventSettings.allowEditing = false
-    }
-  }, [])
-
-  const onAction = useCallback(async (event) => {
-    switch (event.requestType) {
-      case 'dateNavigate':
-        event.cancel = true
-        break
-    }
-  }, [])
 
   useEffect(() => {
     (async () => {
@@ -105,21 +84,25 @@ export function Agenda({ schedule: scheduleId }: ReservationDefinition) {
 
   return (
     <MainWrapper>
-      <CustomScheduleComponent
-        width='100%'
-        height='auto'
-        eventSettings={{ dataSource: getEventSettings() }}
-        popupOpen={handleEvent}
-        workDays={workingDays()}
-        currentView='WorkWeek'
-        actionComplete={onAction}
-        ref={scheduleRef => handleConfig(scheduleRef)}
+      <AgendaDescription>
+        <h4>Horários de funcionamento</h4>
+        {workingDays()}
+      </AgendaDescription>
+      <CreateEventModal
+        visible={visible}
+        onCancel={() => setVisible(false)}
       >
-        <ViewsDirective>
-          <ViewDirective option='WorkWeek' />
-        </ViewsDirective>
-        <Inject services={[WorkWeek]} />
-      </CustomScheduleComponent>
+        Olá
+      </CreateEventModal>
+      <CustomAgenda
+        minDate={DateTime.now().toJSDate()}
+        maxDate={DateTime.now().plus({ months: 2 }).toJSDate()}
+        items={getCurrentReservations()}
+        itemColos={colors}
+        onCellSelect={createReservation}
+        rowsPerHour={1}
+        locale='pt'
+      />
     </MainWrapper>
   )
 }
--- src/app/Establishment/components/Agenda/styles.ts
+++ src/app/Establishment/components/Agenda/styles.ts
@@ -1,13 +1,47 @@
-import { ScheduleComponent } from '@syncfusion/ej2-react-schedule'
 import styled from 'styled-components'
+import { ReactAgenda } from 'react-agenda';
+import { Modal } from 'antd';
 
 export const MainWrapper = styled.div`
-  overflow-y: auto;
-  overflow-x: auto;
+  width: 100%;
+  
   background-color: white;
   padding: 2vw;
+
+  overflow-y: auto;
+
+  .agenda__table {
+    overflow-y: hidden !important;
+    height: 100%;
+  }
 `
 
-export const CustomScheduleComponent = styled(ScheduleComponent)`
+export const AgendaDescription = styled.div`
+  width: 100%;
+  text-align: center;
   display: flex;
+  flex-direction: column;
+  margin-bottom: 2vw;
+  
+  h4 {
+    font-weight: 600;
+  }
+`
+
+export const WorkingDayDescription = styled.div`
+  border-top: 1px black solid;
+
+  padding: 0.250em;
+  
+  p {
+    margin-bottom: 0 !important;
+  }
+
+`
+
+export const CustomAgenda = styled(ReactAgenda)`
+  max-height: 100%;
+`
+
+export const CreateEventModal = styled(Modal)`
 `
