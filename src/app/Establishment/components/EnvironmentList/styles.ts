6a05761de5e96c0fedaa9ca59f5bd84681a27342
Emerson Valentim <emevalentim0@gmail.com>
2021-10-02 18:57:58 -0300

    feat: update styles to show selected card

--- src/app/Establishment/components/EnvironmentList/styles.ts
+++ src/app/Establishment/components/EnvironmentList/styles.ts
@@ -4,6 +4,13 @@ import styled from 'styled-components'
 export const EnvironmentCard = styled(Card)`
   min-width: 47%;
   margin-bottom: 1vh;
+
+  :hover {
+    cursor: pointer;
+  }
+
+  box-shadow: ${props => props.selected ? '0 4px 8px 0 rgba(0,0,0,0.3)' : 'none'};
+  transition: 0.5s;
 `
 
 export const EnvironmentCardBody = styled.div`
