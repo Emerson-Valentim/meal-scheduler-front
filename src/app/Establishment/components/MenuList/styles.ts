6140eef45fd4395e7c3501313607bdd1916ef7d6
Emerson Valentim <emevalentim0@gmail.com>
2021-10-02 11:36:08 -0300

    feat: add max card width

--- src/app/Establishment/components/MenuList/styles.ts
+++ src/app/Establishment/components/MenuList/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components'
 
 
 export const MenuCard = styled(Card)`
-  
+  max-width: 40%;
 `
--- src/app/Establishment/components/TableList/styles.ts
+++ src/app/Establishment/components/TableList/styles.ts
@@ -2,5 +2,5 @@ import { Card } from 'antd'
 import styled from 'styled-components'
 
 export const TableCard = styled(Card)`
-
+  max-width: 40%;
 `
