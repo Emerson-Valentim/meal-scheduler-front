c01bda80d7ecc4f36f6f92c397b17d44e2ddf388
Emerson Valentim <emevalentim0@gmail.com>
2021-10-11 15:43:46 -0300

    feat: add display flex on action buttons

--- src/app/ConfigPanel/components/Configure/components/ActionButton/ActionButton.tsx
+++ src/app/ConfigPanel/components/Configure/components/ActionButton/ActionButton.tsx
@@ -1,6 +1,8 @@
-import { Button } from 'antd'
 import React from 'react'
+
+import { Button } from 'antd'
 import { RiDeleteBin7Line, RiEditBoxLine } from 'react-icons/ri'
+import { ButtonWrapper } from './styles'
 
 type ActionButtonParams = {
   onEdit: (params?: any) => void
@@ -9,9 +11,9 @@ type ActionButtonParams = {
 
 export function ActionButton({ onEdit, onDelete }: ActionButtonParams): JSX.Element {
   return (
-    <div>
+    <ButtonWrapper>
       <Button onClick={onEdit} icon={<RiEditBoxLine/>}/>
       <Button onClick={onDelete} icon={<RiDeleteBin7Line/>}/>
-    </div>
+    </ButtonWrapper>
   )
 }
---
+++ src/app/ConfigPanel/components/Configure/components/ActionButton/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  
+  display: flex;
+  justify-content: center;
+
+  button {
+    margin: 2px;
+  }
+`
